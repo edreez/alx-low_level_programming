2a00e1274f41d5d1edeba1a2ab6f4a143c602cd2
edreez <olawunmiolatunji77@gmail.com>
2022-03-15 21:20:32 +0000

    using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlb.h>
+/**
+ * main - put char
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}

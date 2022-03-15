7463d5808a2065b470f6a9487970c37191594a4d
edreez <olawunmiolatunji77@gmail.com>
2022-03-15 02:27:39 +0000

    editing printing alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,19 @@
  * main - prints alphabet
  *
  *  Description: alphabet in lowercase
+ *
  *  Return: 0
  */
 int main(void)
 {
 	char a = 'a';
+
 	while (a <= 'z')
 	{
 		putchar(a);
 		a++;
 	}
+
 	putchar('\n');
 	return (0);
 }

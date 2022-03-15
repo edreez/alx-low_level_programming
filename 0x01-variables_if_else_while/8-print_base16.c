90b2aaa4f3cd1f275c0d5d32368898a8ee6b43dc
edreez <olawunmiolatunji77@gmail.com>
2022-03-15 03:21:12 +0000

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints number
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+		putchar(x);
+	
+	for (x = 'a'; x <= 'f'; x++)
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}

d34384e4fcddad12cb33b92e1a07589b9c93b50c
edreez <olawunmiolatunji77@gmail.com>
2022-03-15 21:36:17 +0000

    using putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <holberton.h>
-#include <stdlb.h>
 /**
  * main - print putchar
  *
@@ -8,13 +6,15 @@
  */
 int main (void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
 	_putchar('h');
-	_putchar('a');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);

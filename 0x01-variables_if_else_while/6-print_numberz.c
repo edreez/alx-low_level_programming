45d421be1054a590cbbea0980c9e2515e0bedf1b
edreez <olawunmiolatunji77@gmail.com>
2022-03-15 03:07:55 +0000

    under 10 number without char input

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints int
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+
+	putchar('\n');
+	return (0);
+}

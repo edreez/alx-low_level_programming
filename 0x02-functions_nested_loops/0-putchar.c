d435eec3c913074fb1513908c446113d5fa825c4
edreez <olawunmiolatunji77@gmail.com>
2022-03-16 02:30:28 +0000

    using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('s');
+	_putchar('c');
+	_putchar('h');
+	_putchar('o');
+	_putchar('0');
+	_putchar('l');
+	_putchar('\n');
+
+	return (0);
+}

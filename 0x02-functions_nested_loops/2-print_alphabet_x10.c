eedabf813149de871ebda203f64cc795c9f93505
edreez <olawunmiolatunji77@gmail.com>
2022-03-17 04:27:51 +0000

    printing alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,17 @@
  *
  */
 void print_alphabet_x10(void)
-{ 
+{
 	int count;
 	int x;
 	count = 0;
+
 	while (count < 10)
 	{
 		for (x = 97; x <= 122; x++)
 			_putchar(x);
 		_putchar('\n');
+
 		count++;
 	}
 }

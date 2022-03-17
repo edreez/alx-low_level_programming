df7ebc3976b465817dafbb47ebe91365f6a9b146
edreez <olawunmiolatunji77@gmail.com>
2022-03-17 04:20:22 +0000

    printing alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,12 @@
  *  print_alphabet_x10 - print the alphabet 10 times
  *
  */
-
 void print_alphabet_x10(void)
 { 
 	int count;
 	int x;
 
 	count = 0;
-
 	while (count < 10)
 	{
 		for (x = 97; x <= 122; x++)

323eab5dca96995f55ef8507a401dd4251fab66b
edreez <olawunmiolatunji77@gmail.com>
2022-03-22 03:14:39 +0000

    printing rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,27 @@
 #include "main.h"
 
-int main(void)
-{
-	char *str;
+/**
+ * print_rev - print reverse
+ * @s: input string
+ *
+ * Description: prints a string, in reverse
+ * Return: Always (0)
+ */
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-	return (0);
-}
+void print_rev(char *s)
+{
+	int left, len, i;
+
+	len = 0;
+	left = 0;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		len = i;
+	}
+	for(; len >= left; len-- )
+	{
+		_putchar('\n');
+	}
+
+	

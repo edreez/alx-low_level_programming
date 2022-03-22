cdf1f78a1992c37a803ecb2822cebfbb0d496f74
edreez <olawunmiolatunji77@gmail.com>
2022-03-22 01:45:48 +0000

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns lenght of string
+ * @s: input parameter
+ *
+ * Description: uses pointers
+ * REturn: Always (0)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+		continue;
+	}
+
+	return (i);
+}

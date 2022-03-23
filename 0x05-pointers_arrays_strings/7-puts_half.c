8456fc380591d2c6b959b95473f73805b3bff88a
edreez <olawunmiolatunji77@gmail.com>
2022-03-23 03:30:06 +0000

    printing half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,31 +10,27 @@
 
 void puts_half(char *str)
 {
-	int i;
-	int diff;
+	int i, length, half;
 
-	while (str[i] != '\0')
+	length = 0;
+	while (str[length] != '\0')
 	{
-		i++;
+		length++;
 	}
-	if (i % 2 == 1)
+	if (length % 2 == 0)
 	{
-		diff = (i + 1) / 2;
-
-		while (str[diff] != '\0')
+		half = length / 2;
+		for (i = half; i < length; i++)
 		{
-			_putchar(str[diff]);
-			diff++;
+			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		diff = (i / 2);
-
-		while (str[diff] != '\0')
+		half = (length - 1) / 2;
+		for (i = half + 1; i < length; i++)
 		{
-			_putchar(str[diff]);
-			diff++;
+			_putchar(str[i]);
 		}
 	}
 	_putchar('\n');

fcb4123e5da6d40003a3b26958e26b84a99a9432
edreez <olawunmiolatunji77@gmail.com>
2022-03-27 07:01:28 +0000

    check uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,19 @@
 
 /**
  * _isupper - checks if c is an uppercase letter
+ *
  * @c: integer to test
  * REturn: 1 for true, 0 for false
  */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
+	else
+	{
 	return (0);
+	}
 }

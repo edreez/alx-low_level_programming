12e5c8f985102f61e6db32e6557ff4698dc41795
edreez <olawunmiolatunji77@gmail.com>
2022-03-27 09:28:13 +0000

    print numbers except 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,26 +11,19 @@
 
 void print_most_numbers(void)
 {
-	/*
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 && i !=4)
+		if (i == 2 || i == 4)
 		{
-			_putchar('0' + i);
+			continue;
+		}
+		else
+		{
+			_putchar(i + '0');
 		}
 
 	}
-	_putchar('\n');*/
-	int x;
-
-		for (x = 48; x < 58; x++)
-				{
-							if (x == 50 ||  x == 52)
-											continue;
-									else
-													_putchar(x);
-										}
-			_putchar('\n');
+	_putchar('\n');
 }

85214f4189495dfb83e28330c4da63af9da324af
edreez <olawunmiolatunji77@gmail.com>
2022-04-02 21:49:15 +0000

    print string with _putchar

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* puts_recursion - using _putchar to print a string
+* 
+* Return: Always 0
+*/
+
+void _puts_recursion(char *s)
+{
+	/* Base Case */
+
+	if (*s != '\0'){
+		_putchar (*s);
+		  _puts_recursion( s + 1);
+		}
+
+	else
+	{
+		_putchar('\n');
+	}
+}

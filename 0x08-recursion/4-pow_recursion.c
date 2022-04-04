4463a1181637ad3efb864b769bdede8fdbede012
edreez <olawunmiolatunji77@gmail.com>
2022-04-04 16:06:17 +0000

    power of a number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
 * _pow_recursion: print the value of a number
+*
 * raised to the power of another number
 *_pow_recursion: to return negative value when y is less than 0
 *
 * @x: given integer
 * @y: given integer
-* _pow_recursion: returning the power of a given integer.
-*Return : -1 if y is less than 0, the value of x and y if equal or greater than 1
+* Return: -1 if y is less than 0,
+* the value of x and y if equal or greater than 1 
 */
 int _pow_recursion(int x, int y)
 {
@@ -22,6 +23,6 @@ int _pow_recursion(int x, int y)
 	}
 	else
 	{
-		return (x * _pow_recursion (x, y-1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }

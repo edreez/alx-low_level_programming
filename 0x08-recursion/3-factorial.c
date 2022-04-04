0588784ac70990e5abbc56e85ba05ec3a68c1d18
edreez <olawunmiolatunji77@gmail.com>
2022-04-04 17:19:24 +0000

    print factorial of an integer

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
 * factorial - print the factorial of a number
-*facorial: to return negative value when it is less than 0
+* facorial: to return negative value when it is less than 0
 *
-*@n: given integer
+* @n: given integer
 * factorial: returning the factorial of a given integer.
-*Return : 1 if n is 1 & 0, -1 if n is less than 0
+* Return: 1 if n is 1 & 0, -1 if n is less than 0
 */
 int factorial(int n)
 {

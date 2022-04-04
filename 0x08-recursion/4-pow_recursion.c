d5c375ff1a396883e287d38ebdd12e526929718c
edreez <olawunmiolatunji77@gmail.com>
2022-04-04 17:10:49 +0000

    print power of an integer

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* _pow_recursion: print the value of a number.
+* _pow_recursion - print the value of a number.
 *
 * Description: print power of a given integer.
 * raised to the power of another number
@@ -9,7 +9,7 @@
 * @x: given integer
 * @y: given integer
 * Return: -1 if y is less than 0
-* the value of x and y if equal or greater than 1. 
+* the value of x and y if equal or greater than 1.
 */
 int _pow_recursion(int x, int y)
 {

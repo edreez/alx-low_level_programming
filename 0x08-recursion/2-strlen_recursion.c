5aec501364f0d6af90fa8484e0881ade89146d20
edreez <olawunmiolatunji77@gmail.com>
2022-04-04 17:28:29 +0000

    print length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 /**
 * _strlen_recursion - using _putchar to print length  of a string.
 * @s:string details
-* _strlen_recursion: to print length of a string
+* _strlen_recursion - to print length of a string
 * Return: Always 0.
 */
 int _strlen_recursion(char *s)

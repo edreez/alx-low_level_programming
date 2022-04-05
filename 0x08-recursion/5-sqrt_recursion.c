2b566fbddd377f74773674b6e4c553b2d6a564a4
edreez <olawunmiolatunji77@gmail.com>
2022-04-05 23:58:54 +0000

    print prime number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* _sqrt_recursion - print the square root of a number.
+*
+* Description: print square root of a given integer.
+*_sqrt_recursion: to return negative value when n has no root
+* @n: given integer
+* @m: given integer
+* Return: -1 if n is not equal to power of n
+* the value of x and y if equal or greater than 1.
+*/
+int _sqrt_recursion(int n)
+{
+	int m;
+
+	/* Base Case */
+	if (n == m / 2)
+	{
+		return (1);
+	}
+	if (n * n != m)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_recursion(n ^ 2));
+	}
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* is_prime_number - print the value of a number.
+* Description: print prime number of a given integer.
+* raised to the power of another number
+* is_prime_number: to return negative value when y is less than 0
+* @n: given integer
+* @i: given integer
+* Return: -1 if y is less than 0
+* the value of x and y if equal or greater than 1.
+*/
+int is_prime_number(int n)/* int i */
+{
+	/* base case */
+	int i;
+
+	for (i = 2; i <= n / 2; i++)
+	{
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	else
+        	{
+		return (1);
+        	}
+	return (is_prime_number(n));
+	}
+	return (1);
+}

d93b472e8e38abce1c3357f625cad29924e3ce4c
edreez <olawunmiolatunji77@gmail.com>
2022-04-06 14:22:09 +0000

    print square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,51 @@
 #include "main.h"
 /**
-* sqrt_recursion - print the square root of a number.
+* test_power - print the power of a number.
 *
-* Description: print square root of a given integer.
-* sqrt_recursion: to return negative value when n has no root
-* @n: given integer
-* @m: given integer
+* Description: print power of a given integer.
+* test_power - to return negative value when n has no root
+* @x: given integer
+* @y: given integer
 * Return: -1 if n is not equal to power of n
 * the value of x and y if equal or greater than 1.
 */
-int _sqrt_recursion(int n)
+int test_power(int x, int y)
 {
-	int m;
-
-	/* Base Case */
-	if (n == m / 2)
+	if (x * x == y)
 	{
-		return (1);
+		return (x);
 	}
-	if (n * n != m)
+	else if (x * x > x)
 	{
 		return (-1);
 	}
 	else
 	{
-		return (_sqrt_recursion(n ^ 2));
+		return (0 + test_power(y, (x + 1)));
+	}
+}
+ #include "main.h"
+/**
+* _sqrt_recursion - print the square root of a number.
+*
+* Description: print square root of a given integer.
+* _sqrt_recursion - to return negative value when n has no root
+* @n: given integer
+* Return: -1 if n is not equal to power of n
+* the value of n if equal or greater than 1.
+*/
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	else if (n < 1)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (test_power(2, n));
 	}
 }

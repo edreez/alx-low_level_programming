3eda89c717ccc5717c300e8976f6bd54f78d3c22
edreez <olawunmiolatunji77@gmail.com>
2022-04-24 14:07:34 +0000

    new memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _strdup - main point
+* @str: string to duplicate
+* @n: integer to add
+*Return: pointer to the duplicate string
+*/
+char *_strdup(char *str)
+{
+	char *new;
+	unsigned int i;
+	int n;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (n = 0; str[n] != '\0'; n++)
+
+	new = (char *) malloc(n + 1 * sizeof(char));
+	if (new != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+			new[i] = str[i];
+	}
+	else
+	{
+		return (NULL);
+	}
+	new[i] = '\0';
+	return (new);
+}

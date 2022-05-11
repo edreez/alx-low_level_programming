4423a46c131af891fd9d4dbd85b7be0dcf7b9b57
edreez <olawunmiolatunji77@gmail.com>
2022-05-11 21:27:55 +0000

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,23 +3,37 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 /**
 * main - read only file
 *
 * Return: Always 0
 */
 
-int main(void)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-
-	fd = open("myname.txt", O_CREAT | O_WRONLY, 0600);
-	if (fd == -1 && "myfile.txt" == NULL)
+	char *buf;
+	ssize_t r, w;
+	
+	if (!filename)
 	{
 		return (0);
 	}
 
-	write(fd, "Beautiful World\n", 17);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == 0)
+		return (0);
+	r = read(fd, buf, letters);
+
+	w = write(STDOUT_FILENO, buf, r);
 	close(fd);
-	return (0);
+	free(buf);
+	return (w);
 }
